e27e3160499e7789f9bf7a05242a1b5eec53ef14
Takahiro Kato <takahiro.kato@g.softbank.co.jp>
2014-01-08 18:45:53 +0900

    app_back2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var pass = require('./routes/pass');
+//var pass = require('./routes/pass');
 var http = require('http');
 var path = require('path');
 
