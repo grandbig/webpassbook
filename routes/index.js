bcba6363661bcba18b85c1f878eb9d2999029b68
Takahiro Kato <takahiro.kato@g.softbank.co.jp>
2014-01-08 21:05:40 +0900

    indexjs_fix

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,6 @@
  * GET home page.
  */
 
-var request = require('request');
-var url = "http://fy11-dev.cloudapp.net/hoge";
-var params = {};
-
 exports.index = function(req, res){
-
-	request.post({
-		url: url,
-		form: {
-			params: {uid: "1204263"}
-		}
-	}, function(err, response, body) {
-		res.render('index', { title: 'Express' });
-		console.log("ok");
-  	});
+	res.render('index', { title: 'Express' });
 };
