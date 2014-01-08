c3e1b09c120e0701bc270e0ccaaeda6ba0e66388
Takahiro Kato <takahiro.kato@g.softbank.co.jp>
2014-01-08 18:43:06 +0900

    app_back

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.post('/sendParam/v1/devices/:a/registrations/:b/:c', pass.sendData);
+//app.post('/sendParam/v1/devices/:a/registrations/:b/:c', pass.sendData);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
